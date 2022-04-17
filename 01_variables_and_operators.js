d2dc14ed026824c4708b70fb0ec06132d54ab5f4
Gabriel sierra <101214100+Gabriel5050@users.noreply.github.com>
2022-04-17 16:24:55 -0300

    Add files via upload

---
+++ 01_variables_and_operators.js
@@ -0,0 +1,39 @@
+/* Exercise 1: Variables and Operatos*/
+
+/*
+    create 2 numeric variables and use the operator to save the sum
+    of both numers in a third variable
+*/
+
+console.log('Exercise 1-a');
+
+var numberA = 4;
+var numberB = 3;
+var addition = numberA + numberB;
+console.log(addition);
+
+/*
+    create 2 variables string type and concatenate them
+    saving the result in a third variable
+*/
+
+console.log('Exercise 1-b');
+
+var firstName = 'gabriel';
+var lastName = 'sierra';
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+/*
+    create two variables string type and sum the length of each variable
+    (amount of letters od the string) saving the result of the sum on a 
+    third variable(use kength).
+*/
+
+console.log('Exercise 1-c');
+
+var string_A = 'let´s see if this works';
+var string_B = "properly";
+var additionString = string_A.length + string_B.length;
+console.log(additionString);
+
